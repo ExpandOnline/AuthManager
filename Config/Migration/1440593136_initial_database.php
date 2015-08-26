85432f5275e6e3ab95feb4e9ec8de74bba6c5fcd
Ron <ron_oudgenoeg@hotmail.com>
2015-08-26 17:32:59 +0200

    S56.7 added automatic inserts to migration script

--- Config/Migration/1440593136_initial_database.php
+++ Config/Migration/1440593136_initial_database.php
@@ -16,52 +16,6 @@ class InitialDatabase extends CakeMigration {
 	public $migration = array(
 		'up' => array(
 			'create_table' => array(
-				'acos' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
-					'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'model' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'alias' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'lft' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'index'),
-					'rght' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'idx_acos_lft_rght' => array('column' => array('lft', 'rght'), 'unique' => 0),
-						'idx_acos_alias' => array('column' => 'alias', 'unique' => 0),
-						'idx_acos_model_foreign_key' => array('column' => array('model', 'foreign_key'), 'unique' => 0),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				'aros' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
-					'parent_id' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'model' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'foreign_key' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'alias' => array('type' => 'string', 'null' => true, 'default' => null, 'key' => 'index', 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'lft' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'index'),
-					'rght' => array('type' => 'integer', 'null' => true, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'idx_aros_lft_rght' => array('column' => array('lft', 'rght'), 'unique' => 0),
-						'idx_aros_alias' => array('column' => 'alias', 'unique' => 0),
-						'idx_aros_model_foreign_key' => array('column' => array('model', 'foreign_key'), 'unique' => 0),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
-				'aros_acos' => array(
-					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'primary'),
-					'aro_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false, 'key' => 'index'),
-					'aco_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'length' => 10, 'unsigned' => false),
-					'_create' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'_read' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'_update' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'_delete' => array('type' => 'string', 'null' => false, 'default' => '0', 'length' => 2, 'collate' => 'utf8_general_ci', 'charset' => 'utf8'),
-					'indexes' => array(
-						'PRIMARY' => array('column' => 'id', 'unique' => 1),
-						'ARO_ACO_KEY' => array('column' => array('aro_id', 'aco_id'), 'unique' => 1),
-					),
-					'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_general_ci', 'engine' => 'InnoDB'),
-				),
 				'auth_manager_authentication_types' => array(
 					'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => true, 'key' => 'primary'),
 					'name' => array('type' => 'string', 'null' => false, 'default' => null, 'length' => 100, 'collate' => 'latin1_swedish_ci', 'charset' => 'latin1'),
@@ -111,7 +65,7 @@ class InitialDatabase extends CakeMigration {
 		),
 		'down' => array(
 			'drop_table' => array(
-				'acos', 'aros', 'aros_acos', 'auth_manager_authentication_types', 'auth_manager_media_platform_users', 'auth_manager_media_platforms', 'auth_manager_oauth_tokens'
+				'auth_manager_authentication_types', 'auth_manager_media_platform_users', 'auth_manager_media_platforms', 'auth_manager_oauth_tokens'
 			),
 		),
 	);
@@ -133,6 +87,40 @@ class InitialDatabase extends CakeMigration {
  * @return bool Should process continue
  */
 	public function after($direction) {
+		if($direction === "up") {
+			$mediaPlatform = ClassRegistry::init('AuthManager.MediaPlatform');
+			$platforms[0]['MediaPlatform']['name'] = 'Analytics (readonly)';
+			$platforms[0]['MediaPlatform']['authentication_type_id'] = '1';
+			$platforms[0]['MediaPlatform']['created'] = '2015-08-21';
+			$platforms[0]['MediaPlatform']['modified'] = '2015-08-21';
+
+			$platforms[1]['MediaPlatform']['name'] = 'Analytics';
+			$platforms[1]['MediaPlatform']['authentication_type_id'] = '1';
+			$platforms[1]['MediaPlatform']['created'] = '2015-08-21';
+			$platforms[1]['MediaPlatform']['modified'] = '2015-08-21';
+
+			$platforms[2]['MediaPlatform']['name'] = 'Facebook Ads';
+			$platforms[2]['MediaPlatform']['authentication_type_id'] = '1';
+			$platforms[2]['created'] = '2015-08-21';
+			$platforms[2]['modified'] = '2015-08-21';
+
+			$mediaPlatform->create();
+			if($mediaPlatform->saveAll($platforms)) {
+				$this->callback->out('MediaPlatform table has been initialized.');
+			}
+
+			$authenticationType = ClassRegistry::init('AuthManager.AuthenticationType');
+			$type['name'] = 'OAUTH2';
+			$type['created'] = '2015-08-24';
+			$type['modified'] = '2015-08-24';
+
+			$authenticationType->create();
+			if($authenticationType->save($type)){
+				$this->callback->out('AuthenticationTypes table has been initialized');
+			}
+
+
+		}
 		return true;
 	}
 }
