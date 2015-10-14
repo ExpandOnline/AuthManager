547f3a306b023e939d740f432df9a75d12fafbba
Michael <michaelvantricht@gmail.com>
2015-10-14 16:04:24 +0200

    S60.00 Add fixture for MediaPlatformUser.

---
+++ Test/Fixture/MediaPlatformUserFixture.php
@@ -0,0 +1,28 @@
+<?php
+
+/**
+ * Class MediaPlatformUserFixture
+ */
+class MediaPlatformUserFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array('model' => 'AuthManager.MediaPlatformUser');
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => '1',
+			'media_platform_id' => 3,
+			'username' => 'test@test.com'
+		)
+	);
+
+}
