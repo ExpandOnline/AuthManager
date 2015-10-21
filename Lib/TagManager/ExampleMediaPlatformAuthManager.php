cdea8877c039f2634b033202d57c999e7911b0bb
KullTC <svenwitteveen@gmail.com>
2015-10-21 17:13:04 +0200

    Refactor Google Authentication and Tagmanager

--- Lib/TagManager/ExampleMediaPlatformAuthManager.php
+++ Lib/TagManager/ExampleMediaPlatformAuthManager.php
@@ -8,22 +8,22 @@ App::uses('MediaPlatformAuthManager','AuthManager.Lib');
  */
 class WebmasterToolsAuthManager extends GoogleAuthManager {
 
-/**
- * @var string
- */
+	/**
+	 * @var string
+	 */
 	protected $_configFile = null;
 
-/**
- * Set the google client and service.
- */
+	/**
+	 * Set the google client and service.
+	 */
 	public function __construct() {
 		parent::__construct();
 	}
 
-/**
- * Get the username for the authenticated user.
- * @return mixed
- */
+	/**
+	 * Get the username for the authenticated user.
+	 * @return mixed
+	 */
 	protected function _getUserName() {
 		$service = new Google_Service_Plus($this->_client);
 		$user = $service->people->get('me');
@@ -34,31 +34,30 @@ class WebmasterToolsAuthManager extends GoogleAuthManager {
 
 	protected function _getScopes() {
 		return array(
-			Google_Service_Webmasters::WEBMASTERS_READONLY,
-			'http://www.google.com/webmasters/tools/feeds/',
-			Google_Service_Plus::USERINFO_EMAIL
+			Google_Service_Plus::USERINFO_EMAIL,
+			Google_Service_Tagmanager::TAGMANAGER_READONLY,
 		);
 	}
 
-/**
- * Set the Google Service
- */
+	/**
+	 * Set the Google Service
+	 */
 	protected function _setGoogleService() {
-		$this->_service = new Google_Service_Webmasters($this->_client);
+		$this->_service = new Google_Service_Tagmanager($this->_client);
 	}
 
-/**
- * @return int
- */
+	/**
+	 * @return int
+	 */
 	public function _getPlatformId() {
-		return MediaPlatform::TagManager;
+		return MediaPlatform::WEBMASTER_TOOLS;
 	}
 
 
-/**
- * @return string
- */
+	/**
+	 * @return string
+	 */
 	protected function _getConfigFilePath() {
-		return CakePlugin::path('AuthManager') . 'Config' . DS . 'API' . DS . 'webmasterTools.json';
+		return CakePlugin::path('AuthManager') . 'Config' . DS . 'API' . DS . 'tagManager.json';
 	}
 }
