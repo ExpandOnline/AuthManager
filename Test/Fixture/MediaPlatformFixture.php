02ebebec2f199aea051aa7c39400c26fae5edeb4
Michael <michaelvantricht@gmail.com>
2015-11-27 17:12:31 +0100

    S63.17 Add MediaPlatformFixture.

---
+++ Test/Fixture/MediaPlatformFixture.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Class MediaPlatformFixture
+ */
+class MediaPlatformFixture extends CakeTestFixture {
+
+/**
+ * Import
+ *
+ * @var array
+ */
+	public $import = array(
+		'model' => 'AuthManager.MediaPlatform',
+		'records' => true
+	);
+
+}
